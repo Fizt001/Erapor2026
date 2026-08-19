37b1fdfdea183f35bdb91739c49d7b9025ccab8c
Fizt001 <hafis.ytd@gmail.com>
2026-08-19 12:04:39 +0700

    chore: trigger backend deployment

--- erapor-api/app/Http/Controllers/Api/AuthController.php
+++ erapor-api/app/Http/Controllers/Api/AuthController.php
@@ -70,6 +70,7 @@ class AuthController extends Controller
                     'role' => $user->role,
                     'is_pengampu_umum' => $user->is_pengampu_umum,
                     'is_pengampu_kejuruan' => $user->is_pengampu_kejuruan,
+                    // Pengecekan role ganda Guru & Walas
                     'is_walas' => $user->isWalas(),
                     'nis' => $nis
                 ]
