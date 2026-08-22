bf2ada3ff269527527f655d3d68dbd96a9846cd8
Fizt001 <hafis.ytd@gmail.com>
2026-08-22 15:48:24 +0700

    fix(api): fix clear-cache.php facade root error

--- erapor-api/public/clear-cache.php
+++ erapor-api/public/clear-cache.php
@@ -14,7 +14,8 @@ try {
     require __DIR__.'/../vendor/autoload.php';
     $app = require_once __DIR__.'/../bootstrap/app.php';
     
-    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+    $kernel->bootstrap();
     
     Illuminate\Support\Facades\Artisan::call('optimize:clear');
     
